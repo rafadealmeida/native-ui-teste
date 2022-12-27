db1e9ee07fb1ac1acbb501f7000ed6b3c25dfdc4
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-12-27 12:01:44 -0300

    Criando página de novo usuário

--- my-app/src/pages/NewAccount/index.jsx
+++ my-app/src/pages/NewAccount/index.jsx
@@ -23,25 +23,24 @@ GoogleSignin.configure({
   webClientId: '',
 });
 
-export default function NewAccount() {
+export default function NewAccount({navigation}) {
   const [load, setLoad] = useState(false);
   const [show, setShow] = useState(false);
-
-  const handleLoad = () => {
-    setLoad(true);
-    setTimeout(() => {
-      setLoad(false);
-    }, 5000);
-  };
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
   const handleNewAccount = () => {
     setLoad(true);
     auth()
-      .createUserWithEmailAndPassword('teste@email.com', '123456')
-      .then(() => alert('Conta', 'Cadastrado com sucesso!'))
+      .createUserWithEmailAndPassword(email, senha)
+      .then(() => Alert.alert('Conta', 'Cadastrado com sucesso!'))
       .catch((error) => console.log(error))
-      .finally(() => setLoad(false));
+      .finally(() => {
+        setLoad(false);
+        navigation.navigate('Login');
+      });
   };
+
   return (
     <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
       <VStack space={5} alignItems="center">
@@ -54,6 +53,7 @@ export default function NewAccount() {
             md: '25%',
           }}
           InputLeftElement={<Icon as={<MaterialIcons name="person" />} size={5} ml="2" color="muted.400" />}
+          onChangeText={(text) => setEmail(text)}
         />
         <Input
           mx="3"
@@ -74,28 +74,9 @@ export default function NewAccount() {
             </Pressable>
           }
           placeholder="Senha"
+          onChangeText={(text) => setSenha(text)}
         />
-        <Input
-          mx="3"
-          w={{
-            base: '75%',
-            md: '25%',
-          }}
-          type={show ? 'text' : 'password'}
-          InputLeftElement={<Icon as={<MaterialIcons name="lock" />} size={5} ml="2" color="muted.400" />}
-          InputRightElement={
-            <Pressable onPress={() => setShow(!show)}>
-              <Icon
-                as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
-                size={5}
-                mr="2"
-                color="muted.400"
-              />
-            </Pressable>
-          }
-          placeholder="Confirmar senha"
-        />
-        <Button isLoading={load} isLoadingText="Entrando...." variant="outline" onPress={handleLoad}>
+        <Button isLoading={load} isLoadingText="Entrando...." variant="outline" onPress={handleNewAccount}>
           Criar conta
         </Button>
       </VStack>
